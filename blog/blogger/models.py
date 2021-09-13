685bb11af7113af6c9c6970ea8ed8913c3a0d895
HusseinOkasha <husseinokasha13@gmail.com>
2021-09-13 15:56:48 +0200

    making the email field in blogger model unique

--- blog/blogger/models.py
+++ blog/blogger/models.py
@@ -4,6 +4,6 @@ class blogger(models.Model):
     first_name = models.CharField(max_length=64)
     last_name = models.CharField(max_length=64)
     username = models.CharField(max_length=128)
-    email = models.EmailField()
+    email = models.EmailField(unique=True)
 
 
