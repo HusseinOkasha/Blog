64ab3693b496246c183bba837e316812ebfc0478
HusseinOkasha <husseinokasha13@gmail.com>
2021-09-13 15:54:17 +0200

    creating post model

--- blog/posts/models.py
+++ blog/posts/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class post(models.Model):
+    date = models.DateTimeField()
+    body = models.TextField(max_length=1000)
+    is_public = models.BooleanField()
+    blogger_username = models.CharField(max_length=128)
