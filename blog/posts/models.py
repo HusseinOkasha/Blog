67ead2ca5e916b63de6533c5210d6dc814d5a7c5
HusseinOkasha <husseinokasha13@gmail.com>
2021-09-16 13:38:43 +0200

    Adding blogger_id field in the database

--- blog/posts/models.py
+++ blog/posts/models.py
@@ -1,7 +1,7 @@
 from django.db import models
-
+from blogger.models import blogger
 class post(models.Model):
     date = models.DateTimeField()
     body = models.TextField(max_length=1000)
     is_public = models.BooleanField()
-    blogger_username = models.CharField(max_length=128)
+    blogger_id = models.ForeignKey(blogger, on_delete=models.CASCADE)
